a547661fbf78eb3d8b9aa77c19ca2c0a56267fe0
kamiyn <kamiyn@live.jp>
2020-05-06 19:05:56 +0900

    add baseDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 import { Configuration } from '@nuxt/types'
 const environment = process.env.NODE_ENV || 'development'
+const baseDir = '/farsinum3'
 
 const config: Configuration = {
   mode: 'spa',
+  router: {
+    base: baseDir + '/'
+  },
   /*
    ** Headers of the page
    */
@@ -11,9 +15,9 @@ const config: Configuration = {
     title: 'FarsiNum on nuxt.js',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: baseDir + '/favicon.ico' }]
   },
   /*
    ** Customize the progress-bar color
@@ -26,26 +30,21 @@ const config: Configuration = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-  ],
+  plugins: [],
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    '@nuxtjs/vuetify',
-    '@nuxt/typescript-build'
-  ],
+  buildModules: ['@nuxtjs/vuetify', '@nuxt/typescript-build'],
   /*
    ** Nuxt.js modules
    */
-  modules: [
-  ],
+  modules: [],
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/variables.scss']
   }
 }
 
